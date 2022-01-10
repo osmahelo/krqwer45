b2e2987a0916d13d933909517e3a98b1768e2776
Oscar <omherreral81@gmail.com>
2022-01-10 17:38:35 -0500

    elimina tareas

--- src/App.js
+++ src/App.js
@@ -27,18 +27,29 @@ function App() {
     }
   }
   
+  const deleteTask = i =>{
+    setTasks(tasks.filter((tasks, j) => i !== j)
+    )
+  } 
+
   const toggleTask = id =>{
     setTasks(tasks.map(tasks => tasks.id === id ? { ...tasks, done: !tasks.done} : tasks )
     )
   }
 
-  
+    
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
           <ul className="todo">
-            {tasks.map((task, index) => <li key={task.id} className={task.done ? 'done' : null} onClick={() => toggleTask(task.id)}>{task.name}</li>)}
+            {tasks.map((task, index) => 
+            <li key={task.id} className={task.done ? 'done' : null}>
+              {task.name}
+              <button onClick={() => toggleTask(task.id)}>{task.done ? "Undone" : "Done"}</button>
+              <button onClick={() => deleteTask(index)}>Delete</button>
+              </li>
+            )}
           </ul>
           <form onSubmit={createTask}>
             <input type="text" id="new-task" className={error ? 'error' : null} placeholder="Ingresa una tarea y oprime Enter" value={newTask} onChange={e => setNewtask(e.target.value)} />
