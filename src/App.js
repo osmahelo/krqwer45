0465594a9fef8ca8336ea317cf274de725261826
Oscar <omherreral81@gmail.com>
2022-01-10 16:03:13 -0500

    useState

--- src/App.js
+++ src/App.js
@@ -1,55 +1,52 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 /// Modifica el componente para que se puedan agregar tareas, tachar y destacharlas y error de validacion en el input
 
 let id = 4;
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      tasks: [
+function App() {
+  
+  const [tasks, setTasks] = useState ([
         { id: 1, name: "Sacar la ropa", done: false },
         { id: 2, name: "Hacer la cama", done: true },
         { id: 3, name: "Leer un rato", done: false }
-      ],
-      newTask: '',
-      error: false
-    }
-    //this.createTask = this.createTask.bind(this)
-  }
+      ]);
+  const [newTask, setNewtask] = useState('');
+  const [error, setError] = useState(false);
+  
+  
 
-  createTask = (e) =>{
+  const createTask = (e) =>{
     e.preventDefault()
-    if (this.state.newTask){
-      this.setState({tasks: this.state.tasks.concat({id, name: this.state.newTask, done: false}), newTask: "", error: false })
-    id ++
+    if (newTask){
+      setTasks([...tasks, {id, name: newTask, done: false}])
+      id ++
+      setNewtask('')
     }
     else{
-      this.setState({error: true});
+      setError(true);
     }
   }
-
-  toggleTask = id =>{
-    this.setState({
-      tasks: this.state.tasks.map(task => task.id === id ? { ...task, done: !task.done} : task )
-    })
+  
+  const toggleTask = id =>{
+    setTasks(tasks.map(tasks => tasks.id === id ? { ...tasks, done: !tasks.done} : tasks )
+    )
   }
 
-  render() {
+  
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
           <ul className="todo">
-            {this.state.tasks.map((task, index) => <li key={task.id} className={task.done ? 'done' : null} onClick={() => this.toggleTask(task.id)}>{task.name}</li>)}
+            {tasks.map((task, index) => <li key={task.id} className={task.done ? 'done' : null} onClick={() => toggleTask(task.id)}>{task.name}</li>)}
           </ul>
-          <form onSubmit={this.createTask}>
-            <input type="text" id="new-task" className={this.state.error ? 'error' : null} placeholder="Ingresa una tarea y oprime Enter" value={this.state.newTask} onChange={e => this.setState({newTask: e.target.value})} />
+          <form onSubmit={createTask}>
+            <input type="text" id="new-task" className={error ? 'error' : null} placeholder="Ingresa una tarea y oprime Enter" value={newTask} onChange={e => setNewtask(e.target.value)} />
           </form>
         </div>
       </div>
     )
   }
-}
+
 
 export default App;
